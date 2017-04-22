68ec21f165d8e7e29b0b4b77ccf667b46822dff4
hiteshkumar111 <hiteshkumar111@gmail.com>
2017-04-22 18:38:11 +0530

    Bug fixed in Remove Person in search

--- src/main/java/com/apple/repos/SearchRepositoryCustom.java
+++ src/main/java/com/apple/repos/SearchRepositoryCustom.java
@@ -11,9 +11,9 @@ import com.apple.entity.UserSolrDocument;
 @Repository
 public interface SearchRepositoryCustom extends SolrCrudRepository<UserSolrDocument, String> {
 
-	List<UserSolrDocument> findUserByEmail(String searchText);
+	//List<UserSolrDocument> findUserByEmail(String searchText);
 	
-	@Query("email:*?0* OR profileid:*?0* OR personid:*?0* OR expertise:*?0* OR location:*?0* OR name:*?0* OR bio:*?0* OR occupation:*?0* OR firstname:*?0* OR lastname:*?0* OR title:*?0*")
+	@Query("company:*?0* OR url:*?0* OR avatarid:*?0* OR expertise:*?0* OR location:*?0* OR name:*?0* OR bio:*?0* OR occupation:*?0* OR title:*?0*")
 	List<UserSolrDocument> findByQueryAnnotation(String searchTerm);
 	
    // Map<String,Object> search(Query query);
