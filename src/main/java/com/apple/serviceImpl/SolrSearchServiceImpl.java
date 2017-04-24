33b16b0b4e6413a8062d130b219318bc6fb3f3b4
hiteshkumar111 <hiteshkumar111@gmail.com>
2017-04-25 00:57:17 +0530

    Bug fixed

--- src/main/java/com/apple/serviceImpl/SolrSearchServiceImpl.java
+++ src/main/java/com/apple/serviceImpl/SolrSearchServiceImpl.java
@@ -3,9 +3,17 @@ package com.apple.serviceImpl;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.apache.solr.client.solrj.beans.Field;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Page;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.mapping.Indexed;
+import org.springframework.data.solr.core.query.Criteria;
 import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleQuery;
 import org.springframework.stereotype.Service;
 
 import com.apple.dtos.request.SearchRequestDTO;
@@ -15,24 +23,41 @@ import com.apple.repos.SearchRepositoryCustom;
 import com.apple.services.SolrSearchService;
 
 @Service
-public class SolrSearchServiceImpl implements SolrSearchService{
-	
-	@Autowired
+public class SolrSearchServiceImpl implements SolrSearchService {
+
+	@Resource
 	private SolrTemplate solrTemplate;
 
 	@Autowired
 	private SearchRepositoryCustom searchRepo;
+
+	public List<UserSolrDocument> search(SearchRequestDTO searchRequest) {
+
+		//String searchKeyword = searchRequest.getSearchText();
+		// List<UserSolrDocument> listOfUsers =
+		// searchRepo.findByCompanyContainsOrExpertiseContainsOrLocationContainsOrOccupationContainsOrTitleContainsOrUrlContainsOrNicknameContainsOrAvatarIdContains(searchKeyword,
+		// searchKeyword, searchKeyword, searchKeyword, searchKeyword,
+		// searchKeyword, searchKeyword, searchKeyword);
+
+		String searchKeyword = searchRequest.getSearchText();
+		return searchRepo.findByQueryAnnotation(searchKeyword);
+
+		
+		/*
+		Criteria query = buildSearchRequest(searchRequest);
+		SimpleQuery search = new SimpleQuery(query);
+
+		solrTemplate.setSolrCore("userDetailsCore");
 	
-	public List<UserSolrDocument> search(SearchRequestDTO searchRequest){
-	
-		List<UserSolrDocument> listOfUsers = searchRepo.findByQueryAnnotation(searchRequest.getSearchText());
 		
-	//	Query query = buildSearchRequest(searchRequest);
-		
-		//@SuppressWarnings("unchecked")
-		//Map<String, Object> result = solrTemplate.queryForObject(query, Map.class);
-		
-		return listOfUsers;
+		Page<UserSolrDocument> results = solrTemplate.queryForPage(search, UserSolrDocument.class);
+		return results.getContent();
+*/
+		// @SuppressWarnings("unchecked")
+		// Map<String, Object> result = solrTemplate.queryForObject(query,
+		// Map.class);
+
+		// return listOfUsers;
 	}
 
 	/**
@@ -40,11 +65,24 @@ public class SolrSearchServiceImpl implements SolrSearchService{
 	 * @param searchRequest
 	 * @return
 	 * 
-	 * Responsible for building Query from Search Request DTO.
+	 * 		Responsible for building Query from Search Request DTO.
 	 */
-	private Query buildSearchRequest(SearchRequestDTO searchRequest) {
+	private Criteria buildSearchRequest(SearchRequestDTO searchRequest) {
 
+		Criteria conditions = null;
+
+		if (null == searchRequest) {
+			return null;
+		}
+		String word = searchRequest.getSearchText();
 		
-		return null;
+		conditions = new Criteria("name").contains(word).or(new Criteria("bio").contains(word))
+				.or(new Criteria("company").contains(word)).or(new Criteria("expertise").contains(word))
+				.or(new Criteria("location").contains(word)).or(new Criteria("occupation").contains(word))
+				.or(new Criteria("title").contains(word)).or(new Criteria("url").contains(word))
+				.or(new Criteria("avatarid").contains(word));
+
+		return conditions;
 	}
+
 }
